ec91f5b5dd4d4e5cf1397e217082116c9a403f25
Harsh Srivastav <112232698+harshustle@users.noreply.github.com>
2025-06-14 14:44:56 +0530

    may be fixed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
     setError('');
     try {
-      const response = await fetch('http://localhost:3000/api/auth/login', {
+      const response = await fetch('https://shop-client-v1kw.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
